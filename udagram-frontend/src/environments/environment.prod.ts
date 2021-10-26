341fbada51cc57295bd907cb0cfd62fa95685ad6
amnesiasd <amnesiasd@gmail.com>
2021-10-26 11:41:43 -0700

    updated api url, removed slash

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagramamnesiasdapi-env.eba-sempz5v2.us-east-1.elasticbeanstalk.com/",
+  apiHost: "http://udagramamnesiasdapi-env.eba-sempz5v2.us-east-1.elasticbeanstalk.com",
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagramamnesiasdapi-env.eba-sempz5v2.us-east-1.elasticbeanstalk.com/'
+  apiHost: 'http://udagramamnesiasdapi-env.eba-sempz5v2.us-east-1.elasticbeanstalk.com'
 };
 
 /*

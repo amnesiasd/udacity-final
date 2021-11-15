abff239481a55ec4b446276ac15aa3bbfc797653
amnesiasd <amnesiasd@gmail.com>
2021-11-15 11:53:34 -0800

    test update

--- udagram-api/src/tests/controllers/v0/users/routes/user.routerSpec.ts
+++ udagram-api/src/tests/controllers/v0/users/routes/user.routerSpec.ts
@@ -6,6 +6,6 @@ const request = supertest(app);
 describe("Test User endpoint", () => {
   it("get user endpoint", async () => {
     const response = await request.get("/api/v0/users");
-    expect(response.status).not.toBe(200);
+    expect(response.status).toBe(200);
   });
 });
